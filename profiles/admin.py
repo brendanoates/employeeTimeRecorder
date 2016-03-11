ee8fc4865aed453c87b94b3bcc021861c752a6fc
brendan <brendan.oates@amadeus.com>
2016-03-11 11:47:57 +0000

    allow HR to update users groups in admin

--- profiles/admin.py
+++ profiles/admin.py
@@ -8,6 +8,12 @@ from profiles.models import EmployeeTimeRecorderUser
 
 class EmployeeTimeRecorderUserAdmin(admin.ModelAdmin):
     filter_horizontal = ('groups', 'user_permissions')
+    normaluser_fields = ['groups',]
 
+    def get_form(self, request, obj=None, **kwargs):
+        if not request.user.is_superuser:
+            self.fields = self.normaluser_fields
+
+        return super(EmployeeTimeRecorderUserAdmin, self).get_form(request, obj, **kwargs)
 
 admin.site.register(EmployeeTimeRecorderUser, EmployeeTimeRecorderUserAdmin)
